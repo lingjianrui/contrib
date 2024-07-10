2914e739464be5491db12d33d9021bf63ed547af
lingjianrui <toiklaun@qq.com>
2024-07-10 14:16:53 +0800

    add timestamp

---
+++ function/datetime/timestamp.go
@@ -0,0 +1,36 @@
+package datetime
+
+import (
+	"github.com/project-flogo/core/data"
+	"github.com/project-flogo/core/support/log"
+	"time"
+
+	"github.com/project-flogo/core/data/expression/function"
+)
+
+type Timestamp struct {
+}
+
+func init() {
+	function.Register(&Timestamp{})
+}
+
+func (s *Timestamp) Name() string {
+	return "timestamp"
+}
+
+func (s *Timestamp) GetCategory() string {
+	return "datetime"
+}
+
+func (s *Timestamp) Sig() (paramTypes []data.Type, isVariadic bool) {
+	return []data.Type{}, false
+}
+
+func (s *Timestamp) Eval(params ...interface{}) (interface{}, error) {
+	log.RootLogger().Debugf("Returns the current timestamp")
+	n := time.Now()
+	unixTimestamp := n.Unix()
+	millisTimestamp := unixTimestamp * 1000
+	return millisTimestamp
+}
